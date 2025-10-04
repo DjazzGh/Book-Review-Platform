e6b4a22904507161179d9563d90c1bb41064f1f7
DjazzGh <djazia.gh77@gmail.com>
2025-10-04 22:02:09 +0100

    fixed frontend for deployment

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
-
+const API_URL = process.env.REACT_API_URL || 'http://localhost:5001/api'
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
